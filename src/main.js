ccb116da174941418d83bcbba8d7632c7b1a0d05
Frank Shi <frshi@ucsc.edu>
2023-10-25 17:58:10 -0700

    set up game window

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// Name: Frank Shi
+// Title: 
+// Hours spent:
+// Creative tilt justification:
+
+'use strict'
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 1280,
+    height: 720,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
+
+let game = new Phaser.Game(config);

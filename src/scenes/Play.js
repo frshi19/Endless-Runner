d0c34f8acf6728842fd077482b8c3583904de824
Frank Shi <frshi@ucsc.edu>
2023-10-30 02:51:21 -0700

    added bloodshield mechanic

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,6 +12,12 @@ class Play extends Phaser.Scene {
 
         this.load.atlas('vampire', './assets/vampire.png', './assets/vampire.json');
         this.load.image('cross', './assets/cross.png')
+        this.load.image('blood', './assets/blood.png')
+        this.load.image('bloodshield', './assets/bloodshield.png')
+        this.load.spritesheet('garlic', './assets/garlic.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
     }
 
     create(){
@@ -83,6 +89,7 @@ class Play extends Phaser.Scene {
         this.player.anims.play('walk', true)
         this.player.setGravityY(2000)
         this.player.setCollideWorldBounds(true)
+        this.player.setImmovable(true)
 
         // enemies
         this.cross = this.physics.add.sprite(game.config.width + 150, game.config.height/2, 'cross').setScale(7).setOrigin(0.5);
@@ -90,7 +97,7 @@ class Play extends Phaser.Scene {
 
 
 
-        // Increase difficulty
+        // Lightning effect and increase difficulty
         this.anims.create({
             key: 'flash',
             frameRate: 12,
@@ -103,16 +110,37 @@ class Play extends Phaser.Scene {
         this.time.addEvent({
             delay: 10000, 
             callback: () => {
-                this.lightning.anims.play('flash', false)
-                speed -= 75
+                if(!this.gameOver){
+                    this.lightning.anims.play('flash', false)
+                    speed -= 75
+                }
             },
             callbackScope:this,
             loop: true
         });
 
+        // add blood at 20 seconds
+        this.blood = this.physics.add.sprite(game.config.width + 150, game.config.height/2, 'blood').setScale(5).setOrigin(0.5);
+        this.blood.body.setSize(16,9);
+        this.time.addEvent({
+            delay: 5000, 
+            callback: () => {
+                if(!this.gameOver){
+                    this.blood.x = game.config.width + this.blood.width * 5
+                    this.blood.y = Phaser.Math.Between(this.blood.height * 5 * 2, game.config.height - this.blood.height * 5)
+                    this.blood.body.velocity.x = speed
+                }
+            },
+            callbackScope:this,
+            loop: true
+        });
+
+
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
+        this.bloodflag = false;
 
         // score config
         let gameOverConfig = {
@@ -131,18 +159,41 @@ class Play extends Phaser.Scene {
 
         // collider
         this.physics.add.collider(this.player, this.cross, (player, cross)=> {
-            player.destroy()
-            cross.destroy()
-            this.gameOver = true
-            this.add.text(game.config.width/2, game.config.height/2 - 72, 'GAME OVER', gameOverConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2, 'Time survived: ' + this.timer/1000 + ' seconds', gameOverConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 72, 'Press (R) to Restart or (ESC) for Menu', gameOverConfig).setOrigin(0.5);
+            if (this.bloodflag){
+                this.bloodflag = false
+                cross.x = -69
+            }else{
+                player.destroy()
+                cross.destroy()
+                this.gameOver = true
+                this.add.text(game.config.width/2, game.config.height/2 - 72, 'GAME OVER', gameOverConfig).setOrigin(0.5);
+                this.add.text(game.config.width/2, game.config.height/2, 'Time survived: ' + this.timer/1000 + ' seconds', gameOverConfig).setOrigin(0.5);
+                this.add.text(game.config.width/2, game.config.height/2 + 72, 'Press (R) to Restart or (ESC) for Menu', gameOverConfig).setOrigin(0.5);
+            }
+            
+        })
+
+        
+        this.bloodshield =  this.add.sprite(game.config.width + 150, game.config.height/2, 'bloodshield').setScale(5).setOrigin(0.5);
+        this.physics.add.collider(this.player, this.blood, (player, blood)=> {
+            blood.x = -69
+            blood.y = -420
+            this.blood.setVelocity(0)
+            this.bloodflag = true;
         })
     }
 
     update(){
         
         if(!this.gameOver){
+            // blood shield manager
+            if (this.bloodflag){
+                this.bloodshield.x = this.player.x
+                this.bloodshield.y = this.player.y
+            }else{
+                this.bloodshield.x = -49
+                this.bloodshield.y = -420
+            }
             // jump checker
             if (this.player.y >= this.game.config.height - 112){
                 this.DoubleJump = 0;
@@ -171,7 +222,11 @@ class Play extends Phaser.Scene {
             // warp cross from left to right
             if (this.cross.x <= 0 - this.cross.width){
                 this.cross.x = game.config.width + this.cross.width * 7
-                this.cross.y = Phaser.Math.Between(this.cross.height * 7 , game.config.height - this.cross.height * 7)
+                this.cross.y = Phaser.Math.Between(game.config.height / 3 + this.cross.height * 7 , game.config.height - this.cross.height * 7)
+            }
+            // blood
+            if (this.blood.x <= 0 - this.blood.width){
+                this.blood.setVelocity(0)
             }
         }else{
             if (Phaser.Input.Keyboard.JustDown(keyR)){

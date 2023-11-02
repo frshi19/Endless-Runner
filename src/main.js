5855c55d63d91bbd92a21a3f2ec30b3a7755ef6e
Frank Shi <frshi@ucsc.edu>
2023-11-02 03:57:36 -0700

    added comment block on main.js for creative tilt

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 // Name: Frank Shi
-// Title: 
-// Hours spent:
+//
+// Title: Manor Run
+//
+// Hours spent: 30
+//
 // Creative tilt justification:
+//
+// Does your game do something technically interesting?
+// At 20 seconds into the game, a garlic obstacle spawns that has varying collider boundaries, 
+// which are associated with certain frames of its animation. This took a lot of time looking outside of class examples
+// and at the Phaser documentation trying to figure out the proper commands, offsets, and also just debugging it.
+// I think this mechanic makes my game unique and I am particularly proud of it. (Line 321, Play.js)
+// 
+// 
+// Does your game have a great visual style?
+// This was my first time having to create multiple visual assets and animations for a game, and I have to say I feel
+// proud of how it turned out. I am also proud of the looping background music and its
+// baroque era/8bit aesthetics, which fits perfectly with the vampyric/retro visual themes.
+// I am particulary proud of the lightning animation that occurs every 10 seconds
+// that flashes behind the transparent windows in the scrolling hallway background, which also signals an increase in difficulty.
+// Not only does it serve a purpose to the game design, but also fits well with the theme of the game.
 
 'use strict'
 
